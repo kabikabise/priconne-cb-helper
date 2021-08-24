3977513e08f4779d852c02f8d4484d367e7cb32f
kabikabise <cyronrobles@gmail.com>
2021-08-25 03:48:26 +0800

    Update sheet.js

--- sheet.js
+++ sheet.js
@@ -1,10 +1,17 @@
-function init() {
-          Papa.parse('https://docs.google.com/spreadsheets/d/e/2PACX-1vTOAib0Cy9PAsvgepC5q_gk4PiQpAVV8mm7cEIRak0IcBU4UUk34o6KSmnYeZ8TQWWVe6PubRaC6wQ0/pub?output=csv', {
-          download: true,
-          header: true,
-          complete: function(results) {
-            var data = results.data
-            console.log(data)
-          }
-        })
-window.addEventListener('DOMContentLoaded', init)
+<script src='https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.5.1/tabletop.min.js'></script>
+<script type='text/javascript'>    
+  var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1K5jqUx56lN2z494FsSeYvSs0_BkiC03R9eu30IIkGXs/edit?usp=sharing';
+
+  function init() {
+    Tabletop.init( { key: publicSpreadsheetUrl,
+                     callback: showInfo,
+                     simpleSheet: true } )
+  }
+
+  function showInfo(data, tabletop) {
+    alert('Successfully processed!')
+    console.log(data);
+  }
+
+  window.addEventListener('DOMContentLoaded', init)
+</script>
