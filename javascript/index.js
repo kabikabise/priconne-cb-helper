e8030c1eabad96e0dc6c1bde1bcfa0242f518e17
kabikabise <cyronrobles@gmail.com>
2021-09-10 01:02:42 +0800

    basic working team 1

--- javascript/index.js
+++ javascript/index.js
@@ -14,6 +14,7 @@ function set_team1(){
   document.getElementById('team3').setAttribute("class", "teambox_inner");
   document.getElementById('team3_title').setAttribute("class", "teambox_title");
 }
+
 function set_team2(){
   active_team = 2;
   document.getElementById('team2').setAttribute("class", "teambox_inner_selected");
@@ -25,6 +26,7 @@ function set_team2(){
   document.getElementById('team3').setAttribute("class", "teambox_inner");
   document.getElementById('team3_title').setAttribute("class", "teambox_title");
 }
+
 function set_team3(){
   active_team = 3;
   document.getElementById('team3').setAttribute("class", "teambox_inner_selected");
@@ -36,8 +38,50 @@ function set_team3(){
   document.getElementById('team2').setAttribute("class", "teambox_inner");
   document.getElementById('team2_title').setAttribute("class", "teambox_title");
 }
+
 function set_lima(){
   for (let i = 0; i < team1_selected.length; i++){
+    if (team1_selected[i] == "lima"){
+      var element = document.getElementById("lima");
+      element.classList.add("u_default");
+      element.classList.remove("u_select");
+      for (let j = i; j < team1_selected.length-1; j++){
+        team1_selected[j] = team1_selected[j+1];
+        switch(j){
+          case 0:
+            var x = document.getElementById('t1_1').className;
+            document.getElementById('t1_0').setAttribute("class", x);
+            break;
+          case 1:
+            var x = document.getElementById('t1_2').className;
+            document.getElementById('t1_1').setAttribute("class", x);
+            break;
+          case 2:
+            var x = document.getElementById('t1_3').className;
+            document.getElementById('t1_2').setAttribute("class", x);
+            break;
+        }
+      }
+      switch (i){
+        case 0:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+        case 1:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+        case 2:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+        case 3:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+      }
+      break;
+    }
     if (team1_selected[i] == 0){
       switch (i){
         case 0:
@@ -57,6 +101,290 @@ function set_lima(){
           team1_selected[3] = "lima";
           break;
       }
+      var element = document.getElementById("lima");
+      element.classList.add("u_select");
+      element.classList.remove("u_default");
+      break;
+    }
+  }
+}
+
+
+function set_miyako(){
+  for (let i = 0; i < team1_selected.length; i++){
+    if (team1_selected[i] == "miyako"){
+      var element = document.getElementById("miyako");
+      element.classList.add("u_default");
+      element.classList.remove("u_select");
+      for (let j = i; j < team1_selected.length-1; j++){
+        team1_selected[j] = team1_selected[j+1];
+        switch(j){
+          case 0:
+            var x = document.getElementById('t1_1').className;
+            document.getElementById('t1_0').setAttribute("class", x);
+            break;
+          case 1:
+            var x = document.getElementById('t1_2').className;
+            document.getElementById('t1_1').setAttribute("class", x);
+            break;
+          case 2:
+            var x = document.getElementById('t1_3').className;
+            document.getElementById('t1_2').setAttribute("class", x);
+            break;
+        }
+      }
+      switch (i){
+        case 0:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+        case 1:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+        case 2:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+        case 3:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+      }
+      break;
+    }
+    if (team1_selected[i] == 0){
+      switch (i){
+        case 0:
+          document.getElementById('t1_0').setAttribute("class", "u_miyako");
+          team1_selected[0] = "miyako";
+          break;
+        case 1:
+          document.getElementById('t1_1').setAttribute("class", "u_miyako");
+          team1_selected[1] = "miyako";
+          break;
+        case 2:
+          document.getElementById('t1_2').setAttribute("class", "u_miyako");
+          team1_selected[2] = "miyako";
+          break;
+        case 3:
+          document.getElementById('t1_3').setAttribute("class", "u_miyako");
+          team1_selected[3] = "miyako";
+          break;
+      }
+      var element = document.getElementById("miyako");
+      element.classList.add("u_select");
+      element.classList.remove("u_default");
+      break;
+    }
+  }
+}
+
+function set_kuka(){
+  for (let i = 0; i < team1_selected.length; i++){
+    if (team1_selected[i] == "kuka"){
+      var element = document.getElementById("kuka");
+      element.classList.add("u_default");
+      element.classList.remove("u_select");
+      for (let j = i; j < team1_selected.length-1; j++){
+        team1_selected[j] = team1_selected[j+1];
+        switch(j){
+          case 0:
+            var x = document.getElementById('t1_1').className;
+            document.getElementById('t1_0').setAttribute("class", x);
+            break;
+          case 1:
+            var x = document.getElementById('t1_2').className;
+            document.getElementById('t1_1').setAttribute("class", x);
+            break;
+          case 2:
+            var x = document.getElementById('t1_3').className;
+            document.getElementById('t1_2').setAttribute("class", x);
+            break;
+        }
+      }
+      switch (i){
+        case 0:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+        case 1:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+        case 2:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+        case 3:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+      }
+      break;
+    }
+    if (team1_selected[i] == 0){
+      switch (i){
+        case 0:
+          document.getElementById('t1_0').setAttribute("class", "u_kuka");
+          team1_selected[0] = "kuka";
+          break;
+        case 1:
+          document.getElementById('t1_1').setAttribute("class", "u_kuka");
+          team1_selected[1] = "kuka";
+          break;
+        case 2:
+          document.getElementById('t1_2').setAttribute("class", "u_kuka");
+          team1_selected[2] = "kuka";
+          break;
+        case 3:
+          document.getElementById('t1_3').setAttribute("class", "u_kuka");
+          team1_selected[3] = "kuka";
+          break;
+      }
+      var element = document.getElementById("kuka");
+      element.classList.add("u_select");
+      element.classList.remove("u_default");
+      break;
+    }
+  }
+}
+
+function set_jun(){
+  for (let i = 0; i < team1_selected.length; i++){
+    if (team1_selected[i] == "jun"){
+      var element = document.getElementById("jun");
+      element.classList.add("u_default");
+      element.classList.remove("u_select");
+      for (let j = i; j < team1_selected.length-1; j++){
+        team1_selected[j] = team1_selected[j+1];
+        switch(j){
+          case 0:
+            var x = document.getElementById('t1_1').className;
+            document.getElementById('t1_0').setAttribute("class", x);
+            break;
+          case 1:
+            var x = document.getElementById('t1_2').className;
+            document.getElementById('t1_1').setAttribute("class", x);
+            break;
+          case 2:
+            var x = document.getElementById('t1_3').className;
+            document.getElementById('t1_2').setAttribute("class", x);
+            break;
+        }
+      }
+      switch (i){
+        case 0:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+        case 1:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+        case 2:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+        case 3:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+      }
+      break;
+    }
+    if (team1_selected[i] == 0){
+      switch (i){
+        case 0:
+          document.getElementById('t1_0').setAttribute("class", "u_jun");
+          team1_selected[0] = "jun";
+          break;
+        case 1:
+          document.getElementById('t1_1').setAttribute("class", "u_jun");
+          team1_selected[1] = "jun";
+          break;
+        case 2:
+          document.getElementById('t1_2').setAttribute("class", "u_jun");
+          team1_selected[2] = "jun";
+          break;
+        case 3:
+          document.getElementById('t1_3').setAttribute("class", "u_jun");
+          team1_selected[3] = "jun";
+          break;
+      }
+      var element = document.getElementById("jun");
+      element.classList.add("u_select");
+      element.classList.remove("u_default");
+      break;
+    }
+  }
+}
+
+function set_kaori(){
+  for (let i = 0; i < team1_selected.length; i++){
+    if (team1_selected[i] == "kaori"){
+      var element = document.getElementById("kaori");
+      element.classList.add("u_default");
+      element.classList.remove("u_select");
+      for (let j = i; j < team1_selected.length-1; j++){
+        team1_selected[j] = team1_selected[j+1];
+        switch(j){
+          case 0:
+            var x = document.getElementById('t1_1').className;
+            document.getElementById('t1_0').setAttribute("class", x);
+            break;
+          case 1:
+            var x = document.getElementById('t1_2').className;
+            document.getElementById('t1_1').setAttribute("class", x);
+            break;
+          case 2:
+            var x = document.getElementById('t1_3').className;
+            document.getElementById('t1_2').setAttribute("class", x);
+            break;
+        }
+      }
+      switch (i){
+        case 0:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+        case 1:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+        case 2:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+        case 3:
+          document.getElementById('t1_3').setAttribute("class", "teambox_empty");
+          team1_selected[3] = 0;
+          break;
+      }
+      break;
+    }
+    if (team1_selected[i] == 0){
+      switch (i){
+        case 0:
+          document.getElementById('t1_0').setAttribute("class", "u_kaori");
+          team1_selected[0] = "kaori";
+          break;
+        case 1:
+          document.getElementById('t1_1').setAttribute("class", "u_kaori");
+          team1_selected[1] = "kaori";
+          break;
+        case 2:
+          document.getElementById('t1_2').setAttribute("class", "u_kaori");
+          team1_selected[2] = "kaori";
+          break;
+        case 3:
+          document.getElementById('t1_3').setAttribute("class", "u_kaori");
+          team1_selected[3] = "kaori";
+          break;
+      }
+      var element = document.getElementById("kaori");
+      element.classList.add("u_select");
+      element.classList.remove("u_default");
       break;
     }
   }
