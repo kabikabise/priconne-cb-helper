4848b36499e5ca068e296a822e5e19e045c0d269
kabikabise <cyronrobles@gmail.com>
2021-09-18 01:45:13 +0800

    added to all

--- javascript/index.js
+++ javascript/index.js
@@ -85,6 +85,59 @@ function boss1(){
         if (team3_selected[i] == value.u5){
           availcount[4] += 1;
         }
+        switch (active_team) {
+          case 1:
+            if (sup1_selected == value.u1){
+              availcount[5] += 1;
+            }
+            if (sup1_selected == value.u2){
+              availcount[5] += 1;
+            }
+            if (sup1_selected == value.u3){
+              availcount[5] += 1;
+            }
+            if (sup1_selected == value.u4){
+              availcount[5] += 1;
+            }
+            if (sup1_selected == value.u5){
+              availcount[5] += 1;
+            }
+            break;
+          case 2:
+            if (sup2_selected == value.u1){
+              availcount[5] += 1;
+            }
+            if (sup2_selected == value.u2){
+              availcount[5] += 1;
+            }
+            if (sup2_selected == value.u3){
+              availcount[5] += 1;
+            }
+            if (sup2_selected == value.u4){
+              availcount[5] += 1;
+            }
+            if (sup2_selected == value.u5){
+              availcount[5] += 1;
+            }
+            break;
+          case 3:
+            if (sup3_selected == value.u1){
+              availcount[5] += 1;
+            }
+            if (sup3_selected == value.u2){
+              availcount[5] += 1;
+            }
+            if (sup3_selected == value.u3){
+              availcount[5] += 1;
+            }
+            if (sup3_selected == value.u4){
+              availcount[5] += 1;
+            }
+            if (sup3_selected == value.u5){
+              availcount[5] += 1;
+            }
+            break;
+        }
       }
       var asum = availcount[0] + availcount[1] + availcount[2] + availcount[3] + availcount[4] + availcount[5];
       if (asum > 2){
@@ -437,6 +490,59 @@ function boss4(){
         if (team3_selected[i] == value.u5){
           availcount[4] += 1;
         }
+        switch (active_team) {
+          case 1:
+            if (sup1_selected == value.u1){
+              availcount[5] += 1;
+            }
+            if (sup1_selected == value.u2){
+              availcount[5] += 1;
+            }
+            if (sup1_selected == value.u3){
+              availcount[5] += 1;
+            }
+            if (sup1_selected == value.u4){
+              availcount[5] += 1;
+            }
+            if (sup1_selected == value.u5){
+              availcount[5] += 1;
+            }
+            break;
+          case 2:
+            if (sup2_selected == value.u1){
+              availcount[5] += 1;
+            }
+            if (sup2_selected == value.u2){
+              availcount[5] += 1;
+            }
+            if (sup2_selected == value.u3){
+              availcount[5] += 1;
+            }
+            if (sup2_selected == value.u4){
+              availcount[5] += 1;
+            }
+            if (sup2_selected == value.u5){
+              availcount[5] += 1;
+            }
+            break;
+          case 3:
+            if (sup3_selected == value.u1){
+              availcount[5] += 1;
+            }
+            if (sup3_selected == value.u2){
+              availcount[5] += 1;
+            }
+            if (sup3_selected == value.u3){
+              availcount[5] += 1;
+            }
+            if (sup3_selected == value.u4){
+              availcount[5] += 1;
+            }
+            if (sup3_selected == value.u5){
+              availcount[5] += 1;
+            }
+            break;
+        }
       }
       var asum = availcount[0] + availcount[1] + availcount[2] + availcount[3] + availcount[4] + availcount[5];
       if (asum > 2){
@@ -519,6 +625,59 @@ function boss5(){
         if (team3_selected[i] == value.u5){
           availcount[4] += 1;
         }
+        switch (active_team) {
+          case 1:
+            if (sup1_selected == value.u1){
+              availcount[5] += 1;
+            }
+            if (sup1_selected == value.u2){
+              availcount[5] += 1;
+            }
+            if (sup1_selected == value.u3){
+              availcount[5] += 1;
+            }
+            if (sup1_selected == value.u4){
+              availcount[5] += 1;
+            }
+            if (sup1_selected == value.u5){
+              availcount[5] += 1;
+            }
+            break;
+          case 2:
+            if (sup2_selected == value.u1){
+              availcount[5] += 1;
+            }
+            if (sup2_selected == value.u2){
+              availcount[5] += 1;
+            }
+            if (sup2_selected == value.u3){
+              availcount[5] += 1;
+            }
+            if (sup2_selected == value.u4){
+              availcount[5] += 1;
+            }
+            if (sup2_selected == value.u5){
+              availcount[5] += 1;
+            }
+            break;
+          case 3:
+            if (sup3_selected == value.u1){
+              availcount[5] += 1;
+            }
+            if (sup3_selected == value.u2){
+              availcount[5] += 1;
+            }
+            if (sup3_selected == value.u3){
+              availcount[5] += 1;
+            }
+            if (sup3_selected == value.u4){
+              availcount[5] += 1;
+            }
+            if (sup3_selected == value.u5){
+              availcount[5] += 1;
+            }
+            break;
+        }
       }
       var asum = availcount[0] + availcount[1] + availcount[2] + availcount[3] + availcount[4] + availcount[5];
       if (asum > 2){
@@ -600,6 +759,59 @@ function boss5en(){
         }
         if (team3_selected[i] == value.u5){
           availcount[4] += 1;
+        }
+        switch (active_team) {
+          case 1:
+            if (sup1_selected == value.u1){
+              availcount[5] += 1;
+            }
+            if (sup1_selected == value.u2){
+              availcount[5] += 1;
+            }
+            if (sup1_selected == value.u3){
+              availcount[5] += 1;
+            }
+            if (sup1_selected == value.u4){
+              availcount[5] += 1;
+            }
+            if (sup1_selected == value.u5){
+              availcount[5] += 1;
+            }
+            break;
+          case 2:
+            if (sup2_selected == value.u1){
+              availcount[5] += 1;
+            }
+            if (sup2_selected == value.u2){
+              availcount[5] += 1;
+            }
+            if (sup2_selected == value.u3){
+              availcount[5] += 1;
+            }
+            if (sup2_selected == value.u4){
+              availcount[5] += 1;
+            }
+            if (sup2_selected == value.u5){
+              availcount[5] += 1;
+            }
+            break;
+          case 3:
+            if (sup3_selected == value.u1){
+              availcount[5] += 1;
+            }
+            if (sup3_selected == value.u2){
+              availcount[5] += 1;
+            }
+            if (sup3_selected == value.u3){
+              availcount[5] += 1;
+            }
+            if (sup3_selected == value.u4){
+              availcount[5] += 1;
+            }
+            if (sup3_selected == value.u5){
+              availcount[5] += 1;
+            }
+            break;
         }
       }
       var asum = availcount[0] + availcount[1] + availcount[2] + availcount[3] + availcount[4] + availcount[5];
